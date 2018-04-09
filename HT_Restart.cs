78d9ffa1c151bd9296b99c09d9a20d226f0b9a1d
Jing Chun Tan <jtan1993@gmail.com>
2018-04-09 12:19:01 +0100

    Update HT_Restart.cs

--- HT_Restart.cs
+++ HT_Restart.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class HT_Restart : MonoBehaviour {
 
 	public void OnMouseDown () {
-		Application.LoadLevel (Application.loadedLevel);
+		//Application.LoadLevel (Application.loadedLevel);
+		UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);    
 	}
 }
